862feb37fdba95af28693560447ab6345493a41b
Younesfdj <ferradjiyounes69@gmail.com>
2024-06-20 20:20:56 +0100

    refactor: updated .gitignore and removed unnecessary files

--- bin/index.js
+++ bin/index.js
@@ -1,14 +1,5 @@
-#! /usr/bin/env node
-import yargs from "yargs";
-import { hideBin } from "yargs/helpers";
-import init from "./commands/init/index.js";
-yargs(hideBin(process.argv))
-    .example("xgen init express-app", "Initialize a new xgen project")
-    .command("init", "Initialize a new xgen project", {}, function (argv) {
-    init(argv._);
-})
-    .demandCommand()
-    .strict()
-    .option("h", { alias: "help" })
-    .option("v", { alias: "version" })
-    .help("help").argv;
+#!/usr/bin/env node
+
+import runXgen from "../dist/index.js";
+
+runXgen();
--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,16 @@ import yargs from "yargs";
 import { hideBin } from "yargs/helpers";
 import init from "./commands/init/index.js";
 
-yargs(hideBin(process.argv))
-  .example("xgen init express-app", "Initialize a new xgen project")
-  .command("init", "Initialize a new xgen project", {}, function (argv) {
-    init(argv._);
-  })
-  .demandCommand()
-  .strict()
-  .option("h", { alias: "help" })
-  .option("v", { alias: "version" })
-  .help("help").argv;
+const run = () =>
+  yargs(hideBin(process.argv))
+    .example("xgen init express-app", "Initialize a new xgen project")
+    .command("init", "Initialize a new xgen project", {}, function (argv) {
+      init(argv._);
+    })
+    .demandCommand()
+    .strict()
+    .alias("v", "version")
+    .alias("h", "help")
+    .help("help").argv;
+
+export default run;
